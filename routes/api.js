70f447bf078b44c2019a5d37e62c2a8d39514f03
Steve Golden <sgolden2525@outlook.com>
2019-06-02 23:45:22 -0500

    Added variable functionality
    Added functionality so you can change query and event depending on comment _id

--- routes/api.js
+++ routes/api.js
@@ -12,7 +12,8 @@ const mongoose = require('mongoose');
 var expect = require('chai').expect;
 const Schema = require('mongoose').Schema;
 const shortid = require('shortid');
-const fs = require('fs')
+const fs = require('fs');
+
 
 const __dirnames = 'C:/Users/Steve/Documents/GitHub/message-board-project'
 
@@ -77,15 +78,11 @@ module.exports = function (app) {
       });
       //https://flaviocopes.com/pug/#interpolating-variables-in-pug
       res.render(__dirnames + '/views/board.pug', {title: 'TestingFORTOOLONG'});     
-    })
-
-
-
-    
+    })    
 
   app.route('/api/replies/:board')
-    .post((req, res) => { 
- 
+    .post((req, res) => {
+
       messageBoard.findOneAndUpdate(req.body.thread_id, {
         $push: {
           replies: {
@@ -104,7 +101,71 @@ module.exports = function (app) {
       })
     })
 
+    .put((req, res) => {
+      console.log("thread id: " + req.body.thread_id)
+      console.log("reply id: " + req.body.reply_id)
+
+     
+      messageBoard.findOne({ _id: req.body.thread_id }, 
+        function (err, result) {
+          if (err) {
+            console.log(err)
+          } else {
+            //console.log(result);
+            let index = result.replies.map(function (x, i) {
+              if (x._id === req.body.reply_id) {
+                return i;
+              }//end of if statement
+            })//end of map 
+            const chkNum = JSON.stringify(index)
+            let knwNum = parseInt(chkNum.substring(chkNum.lastIndexOf(',') + 1, chkNum.length - 1));
+            console.log("index: " + knwNum)
+            console.log("result: " + result.replies[0].reported)
 
 
+            const fkIndex = 6
+            let chnQuery = "replies." + fkIndex + "._id"
+            let chnQuery1 = "replies." + fkIndex + ".reported"
+
+             var query = {
+              '_id': req.body.thread_id,
+              [chnQuery]: req.body.reply_id
+            }
+        
+            var event = {
+              [chnQuery1]: true
+            }
+          
+            console.log(query);
+            console.log(event)
+
+          
+            messageBoard.findOneAndUpdate(query,    
+              event,
+              {new: true}, function (err, docs) {
+              if (err) {
+                console.log(err)
+              } else {
+                console.log('no error')
+                console.log("DOCS: " + docs)
+              }
+              })
+         
+            /*
+            result.replies.findOneAndUpdate({
+             {
+               i: {
+                
+               }
+             },  
+              function (err, docs) {
+                console.log(docs)
+              }             
+       
+            })
+            */
+          }//end of else   
+        })//end of err, result funct   
+        })//end of put route  
 
 };
