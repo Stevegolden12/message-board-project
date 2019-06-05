c3eae03c627b15301be6337242403efd22c78e51
Steve Golden <sgolden2525@outlook.com>
2019-06-05 02:34:41 -0500

    delete comment functionality complete and pwd validation
    Added delete comment functionality
    
    Added password validation

--- routes/api.js
+++ routes/api.js
@@ -114,8 +114,8 @@ module.exports = function (app) {
       console.log("thread id: " + req.body.thread_id)
       console.log("reply id: " + req.body.reply_id)
 
-     
-      messageBoard.findOne({ _id: req.body.thread_id }, 
+
+      messageBoard.findOne({ _id: req.body.thread_id },
         function (err, result) {
           if (err) {
             console.log(err)
@@ -126,39 +126,39 @@ module.exports = function (app) {
               }//end of if statement
             })//end of map 
             const lastNum = JSON.stringify(index).length - 1;
-          
+
             const chkNum = JSON.stringify(index).substring(1, lastNum)
-            const arrLoc = parseInt(chkNum.split(',').filter((x,i,a) => {
+            const arrLoc = parseInt(chkNum.split(',').filter((x, i, a) => {
               return x !== 'null'
             }))
-        
-           console.log("chkNum: " + chkNum)    
-           
+
+            console.log("chkNum: " + chkNum)
+
             let chnQuery = "replies." + arrLoc + "._id"
             let chnQuery1 = "replies." + arrLoc + ".reported"
 
-             var query = {
+            var query = {
               '_id': req.body.thread_id,
               [chnQuery]: req.body.reply_id
             }
-        
+
             var event = {
               [chnQuery1]: true
             }
-          
+
             console.log(query);
             console.log(event)
 
-          
-            messageBoard.findOneAndUpdate(query,    
+
+            messageBoard.findOneAndUpdate(query,
               event,
-              {new: true}, function (err, docs) {
-              if (err) {
-                console.log(err)
-              } else {
-                console.log('no error')
-                console.log("DOCS: " + docs)
-              }
+              { new: true }, function (err, docs) {
+                if (err) {
+                  console.log(err)
+                } else {
+                  console.log('no error')
+                  console.log("DOCS: " + docs)
+                }
               })
           }//end of else   
         })//end of err, result funct   
@@ -167,13 +167,11 @@ module.exports = function (app) {
     .delete((req, res) => {
       let threadLoc;
 
-      console.log();
-   
-      messageBoard.findOne({ _id: req.body.thread_id }, function(err, result){
+      messageBoard.findOne({ _id: req.body.thread_id }, function (err, result) {
         if (err) {
           console.log(err)
         } else {
-          console.log("RESULT: " + result)
+          //console.log("RESULT: " + result)
           let index = result.replies.map(function (x, i) {
             if (x._id === req.body.reply_id) {
               return i;
@@ -185,41 +183,18 @@ module.exports = function (app) {
           const arrLoc = parseInt(chkNum.split(',').filter((x, i, a) => {
             return x !== 'null'
           }))
-
-          console.log("arrLoc: " + arrLoc);
-
-          let chnQuery = "result.replies." + arrLoc + "._id"
-          let chnQuery1 = "results.replies." + arrLoc
-          let chnQuery2 = "replies." + arrLoc + ".delete_password"
-          let chnQuery3 = "replies." + arrLoc + "._id"
-          let chnQuery4 = "replies." + arrLoc
-
-          var query = {
-            '_id': req.body.thread_id,
-            [chnQuery]: req.body.reply_id,
-            [chnQuery2]: req.body.delete_password
-          }
-
-          var event = {
-            [chnQuery3]: req.body.reply_id
-          }       
-       
           
-          messageBoard.findOneAndUpdate(query,
-            { $pull: { event } },
-            {new: true},
-            function (err, docs) {
-              if (err) {
-                console.log(err)
-              } else {
-                console.log('no error')
-                console.log("DOCS: " + docs)
-              }
-            })
-            
+          const rmvArr = result.replies[arrLoc]  
 
+          if (result.replies[arrLoc].delete_password == req.body.delete_password) {
+            console.log(rmvArr)
+            rmvArr.remove();
+            result.save();   
+            res.send("Comment deleted")
+          } else {
+            res.send("Incorrect Password")
+          }
         }
       })
     })
-
 };
